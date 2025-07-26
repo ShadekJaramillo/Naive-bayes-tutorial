e4e6c04c1dd5f1db8fb13893a4d97bf2b4ac78a1
Shadek <shadekjaramillo@gmail.com>
2025-07-26 13:33:46 -0500

    added a script to fetch the data for the project.

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,78 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-import pandas as pd
+from pandas import read_csv, read_sql_query
+import sqlite3
+import requests
+from io import StringIO
+import os
 
-# load the .env file variables
-load_dotenv()
+DATA_URL = 'https://raw.githubusercontent.com/4GeeksAcademy/naive-bayes-project-tutorial/main/playstore_reviews.csv'
+
+relative_paths = {
+    'raw_data' : os.path.join('data', 'raw', 'reviews_data'),
+}
+
+paths = {
+    'current_dir' : os.path.dirname(os.path.abspath(__file__)),
+    'root_dir' : os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+}
+
+def csv_to_db_file(csv_data:str):
+    reviews_db_path = os.path.join(paths['root_dir'], relative_paths['raw_data']+'.db')
+    create_db()
+    with sqlite3.connect(reviews_db_path) as connection:
+        try:
+            df = read_csv(StringIO(csv_data))
+            df.to_sql('reviews_data', connection, if_exists='replace')
+            print(f'Data successfully written to database at {reviews_db_path}')
+        except ValueError as e:
+            print(f'An error occurred while adding the table to the database, this is likely because the database already contains that table:\n    {e}')
+
+def load_reviews_db():
+    reviews_db_path = os.path.join(paths['root_dir'], relative_paths['raw_data']+'.db')
+    with sqlite3.connect(reviews_db_path) as connection:
+        try:
+            query = 'SELECT * FROM reviews_data'
+            df = read_sql_query(query, connection, index_col='index')
+            print('Database successfully loaded')
+            return df
+        except Exception as e:
+            print(f'An error occurred while reading the database: {e}')
 
 
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+def save_raw_csv(data:str):
+    reviews_csv_path = os.path.join(paths['root_dir'], relative_paths['raw_data']+'.csv')
+    with open(reviews_csv_path, 'w', encoding='UTF-8') as f:
+        f.write(data)
+    print(f'Data successfully saved to csv in {reviews_csv_path}')
+
+def fetch_data() -> str:   
+    try:
+        with requests.get(DATA_URL, timeout = 20) as response:
+            response.raise_for_status()
+            data = response.text
+            print(f'Data successfully fetched from {DATA_URL[:50]}...')
+            return data
+            
+    except requests.exceptions.HTTPError as e:
+        print(f'Error while fetching the data from {DATA_URL[:50]}...: {e}')
+    
+def create_db():
+    reviews_db_path = os.path.join(paths['root_dir'], relative_paths['raw_data']+'.db')
+    with sqlite3.connect(reviews_db_path) as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS reviews_data(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Package VARCHAR(255),
+            review TEXT
+            polarity BOOL
+            );
+            """
+        )
+        print(f'Database successfully created at {reviews_db_path}')
+
+if __name__ == "__main__":
+    csv_data = fetch_data()
+    save_raw_csv(csv_data)
+    csv_to_db_file(csv_data)
+    
